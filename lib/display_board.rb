47aef4e26a5bf8f1540bc40bd17a560564f3db95
Sophia Fang <sophiaqfang@gmail.com>
2018-06-28 23:59:57 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,9 @@
 # out the current state.
 
 def display_board(board)
-  puts ""
-  puts ""
-  puts ""
-  puts ""
-  puts ""
+  puts " #{board[0]} | #{board[1]} | #{} "
+  puts "-----------"
+  puts " #{} | #{} | #{} "
+  puts "-----------"
+  puts " #{} | #{} | #{} "
 end
