7ad55debfbe62b008042ffdbf066377534f25fec
Sophia Fang <sophiaqfang@gmail.com>
2018-06-28 23:56:47 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,14 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board()
-  
+
+b = []
+
+def display_board(board)
+  puts
+  puts
+  puts
+  puts
+  puts
 end
+
+display_board()
